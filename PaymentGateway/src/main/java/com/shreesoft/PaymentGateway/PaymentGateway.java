9727903741b78ff629d16d292fb0ff4a9dbf2ef5
srihari <srihari@srihari-laptop.(none)>
2012-08-13 00:01:20 +0530

    Hello World

---
+++ PaymentGateway/src/main/java/com/shreesoft/PaymentGateway/PaymentGateway.java
@@ -0,0 +1,13 @@
+package com.shreesoft.PaymentGateway;
+
+/**
+ * Hello world!
+ *
+ */
+public class PaymentGateway 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
